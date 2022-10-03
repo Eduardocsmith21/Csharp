f91bba31e047988567a1ded1c73861944fd60db8
Eduardo Carvalho Smith <eduardocsmith021@gmail.com>
2022-10-02 21:02:30 -0300

    Create IMC.cs

---
+++ IMC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o peso em kg: ");
+            double peso = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a altura em metros:  ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double IMC = peso / (altura * altura);
+
+           if (IMC < 20)
+            {
+                Console.WriteLine("\nIMC = " + IMC + " -> " + " Abaixo do Peso! ");
+            }
+
+           else if ((IMC >= 20) && (IMC <= 24))
+            {
+                Console.WriteLine("\nIMC = " + IMC + "-> " + "Normal!");
+            }
+           else if ((IMC >= 25) && (IMC <= 29))
+            {
+                Console.WriteLine("\nIMC = " + IMC + "-> " + "A cima do Peso!");
+            }
+           else if ((IMC >= 30) && (IMC <= 34))
+            {
+                Console.WriteLine("\nIMC = " + IMC + "-> " + "Obeso!");
+            }
+           else
+            {
+                Console.WriteLine("\nIMC = " + IMC + "-> " + "Muito Obeso!");
+            }
+        }
+    }
+}
